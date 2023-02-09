7d3ec47ed4a6fbc307e37c3fcf6c3f0c9c4a33f4
Oleg Schegol <olschegolev@yahoo.com>
2023-02-09 14:58:55 +0500

    add task 10

--- js/task10.js
+++ js/task10.js
@@ -5,18 +5,52 @@ let textBlock = document.querySelector("#text-block");
 button.addEventListener("click", ButtonClick);
 
 function ButtonClick() {
-  let number = Number(inputText.value);
-
-  if (number == NaN || number < 10 || number > Math.pow(2, 32)) {
-    textBlock.innerHTML = "Неправельно введены данные.";
-    return;
+  let number = String(inputText.value);
+  let data = number.split("/");
+  console.log(data);
+  /*if(data[0]==NaN){
+    console.log("Неправельно введены данные.")
+    textBlock.innerHTML = "Неправельно введены данные."
   }
-  let tmp = number;
-  let invertedNumber = 0;
-  do {
-    invertedNumber = invertedNumber * 10 + (tmp % 10);
-    tmp = Math.floor(tmp / 10);
-  } while (tmp);
-  textBlock.innerHTML =
-    number == invertedNumber ? "Число полиндром" : "Число не полиндром";
+  */
+
+  for (let index = 0; index < data.length; index++) {
+    data[index] = parseInt(data[index]);
+  }
+  /*for (let i = 0; i < data.length; i++) {
+    if(data[i]==NaN){
+      textBlock.innerHTML = "Неправельно введены данные."
+    }
+  }*/
+  if ((data[1] % 2 != 0 && data[1] <= 7) || 
+      (data[1] % 2 == 0 && data[1] > 7)) {
+    if (data[0] < 31) {
+      textBlock.innerHTML = `${data[0] + 1} / ${data[1]} / ${data[2]}`;
+      return;
+    } else if (data[1] != 12) {
+      textBlock.innerHTML = `${1} / ${data[1] + 1} / ${data[2]}`;
+      return;
+    } else {
+      textBlock.innerHTML = `${1} / ${1} / ${data[2] + 1}`;
+      return;
+    }
+  }
+  if (data[1] != 2 && data[0] < 30) {
+    textBlock.innerHTML = `${data[0] + 1} / ${data[1]} / ${data[2]}`;
+    return;
+  } else if (
+    (getBoolLeapYear(data[2]) && data[0] < 29) ||
+    (!getBoolLeapYear(data[2]) && data[0] < 28)
+  ) {    
+      textBlock.innerHTML = `${data[0] + 1} / ${data[1]} / ${data[2]}`;
+      return;
+    }else {
+      textBlock.innerHTML = `${1} / ${data[1] + 1} / ${data[2]}`;
+    }
+}
+
+function getBoolLeapYear(number) {
+  return number % 400 == 0 || (number % 4 == 0 && number % 100 != 0)
+    ? true
+    : false;
 }
--- js/task7.js
+++ js/task7.js
@@ -5,18 +5,22 @@ let textBlock = document.querySelector("#text-block");
 button.addEventListener("click", ButtonClick);
 
 function ButtonClick() {
-  let number = Number(inputText.value);
-
+  let number = parseInt(inputText.value);
+  let discount = 0;
   if (number == NaN || number < 10 || number > Math.pow(2, 32)) {
     textBlock.innerHTML = "Неправельно введены данные.";
     return;
   }
-  let tmp = number;
-  let invertedNumber = 0;
-  do {
-    invertedNumber = invertedNumber * 10 + (tmp % 10);
-    tmp = Math.floor(tmp / 10);
-  } while (tmp);
-  textBlock.innerHTML =
-    number == invertedNumber ? "Число полиндром" : "Число не полиндром";
+  if (number >= 300 && number < 300) {
+    discount = 3;
+  }
+  if (number >= 300 && number < 500) {
+    discount = 5;
+  }
+  if (number >= 500) {
+    discount = 7;
+  }
+  textBlock.innerHTML = `Сумма к оплате ${
+    number - (number / 100) * discount
+  }, скидка составила ${discount}%`;
 }
